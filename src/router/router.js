18b6c29f2dbab016343987cf4a2118ba096a91ca
VitorHugo <vitorwitt.pro@gmail.com>
2023-02-13 18:01:50 -0300

    front md

--- src/router/router.js
+++ src/router/router.js
@@ -4,16 +4,15 @@ import Home from "@/pages/VueHome.vue";
 import DcHeroes from "@/pages/DcHeroes.vue";
 import SignIn from "@/pages/SignIn.vue";
 import SignUp from "@/pages/SignUp.vue";
+import VueMarkdown from "@/pages/VueMarkdown.vue";
 
 const routes = [
   { path: "", component: Home },
   { path: "/sign-in", component: SignIn },
   { path: "/sign-up", component: SignUp },
   { path: "/dc-heroes", component: DcHeroes },
-  {
-    path: "/calendar",
-    component: () => import("../pages/VueCalendar.vue"), // lazy loading, load after clicked
-  },
+  { path: "/calendar", component: () => import("../pages/VueCalendar.vue") },
+  { path: "/markdown", component: VueMarkdown },
 ];
 
 const router = createRouter({
