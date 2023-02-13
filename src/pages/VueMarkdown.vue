37f1421fc8b38380840034ac3c69856099953b5c
VitorHugo <vitorwitt.pro@gmail.com>
2023-02-13 18:01:25 -0300

    front markdown

--- src/pages/VueMarkdown.vue
+++ src/pages/VueMarkdown.vue
@@ -1,8 +1,13 @@
 <template>
-  <section class="w-full">
-    <article class="w-1/2">left</article>
-    <article class="w-1/2">right</article>
-  </section>
+  <div class="flex w-full flex-wrap">
+    <h1 class="w-full text-center my-5 font-bold text-3xl">Markdown App</h1>
+    <section class="w-10/12 flex m-auto h-screen">
+      <article class="w-1/2 border">
+        <textarea class="" name="" id="" cols="30" rows="10"></textarea>
+      </article>
+      <article class="w-1/2 border bg-gray-200">right</article>
+    </section>
+  </div>
 </template>
 
 <script setup></script>
